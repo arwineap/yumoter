6272152d90038fb3e35fc6601930aed6326757b1
aarwine <alex.arwine@evolemediallc.com>
2014-05-06 10:10:44 -0700

    Adding osver to repos. This should be req field

--- json-gen.py
+++ json-gen.py
@@ -7,24 +7,29 @@ reposbasepath = '/home/aarwine/projects/yumoter/repos'
 repos['os-64'] = {}
 repos['os-64']['upstream'] = 'rsync://archive.kernel.org/centos/6.4/os/x86_64/Packages/'
 repos['os-64']['path'] = 'os/6.4'
+repos['os-64']['osver'] = '6.4'
 repos['os-63'] = {}
 repos['os-63']['upstream'] = 'rsync://archive.kernel.org/centos/6.3/os/x86_64/Packages/'
 repos['os-63']['path'] = 'os/6.3'
+repos['os-63']['osver'] = '6.3'
 
 repos['updates-6'] = {}
 repos['updates-6']['upstream'] = 'rsync://mirrors.kernel.org/centos/6/updates/x86_64/Packages/'
 repos['updates-6']['path'] = 'updates/6'
 repos['updates-6']['promotionpath'] = ['wildwest', 'beta', 'live']
+repos['updates-6']['osver'] = '6'
 
 repos['epel-6'] = {}
 repos['epel-6']['upstream'] = 'rsync://mirrors.kernel.org/fedora-epel/6/x86_64/'
 repos['epel-6']['path'] = 'epel/6'
 repos['epel-6']['promotionpath'] = ['wildwest', 'beta', 'live']
+repos['epel-6']['osver'] = '6'
 
 repos['ius-6'] = {}
 repos['ius-6']['upstream'] = 'rsync://mirror.rackspace.com/ius/stable/Redhat/6/x86_64/'
 repos['ius-6']['path'] = 'ius/6'
 repos['ius-6']['promotionpath'] = ['wildwest', 'beta', 'live']
+repos['ius-6']['osver'] = '6'
 
 fh = open('config/repos.json', 'w')
 fh.write(json.dumps(repos, sort_keys=True, indent=4, separators=(',', ': ')))
