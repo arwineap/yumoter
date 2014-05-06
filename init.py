bc92af66b015b9f3e89984b1a7147d035a255f28
aarwine <alex.arwine@evolemediallc.com>
2014-05-06 16:23:11 -0700

    testing pkg types

--- init.py
+++ init.py
@@ -3,7 +3,17 @@ import yumoter
 yumoter = yumoter.yumoter('config/repos.json', '/home/aarwine/git/yumoter/repos')
 
 yumoter.loadRepos("6.4", "wildwest")
-yumoter._returnNewestByNameArch(["httpd"])
+a = yumoter._returnNewestByNameArch(["httpd"])
+print a
+print a.name
+print a.arch
+print a.epoch
+print a.version
+print a.release
+print a.size
+
+
+
 
 #     def loadRepos(self, osVer, env):
 '''
