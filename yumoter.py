de5e9763bcf5aba0d3aacb146f567d3e9b54cac5
aarwine <alex.arwine@evolemediallc.com>
2014-05-07 17:04:19 -0700

    fixing typo, repobasepath not repobasename

--- yumoter.py
+++ yumoter.py
@@ -112,7 +112,7 @@ class yumoter:
         sys.exit(1)
 
     def _pathToUrl(self, path):
-        return "%s/%s" % (self.urlbasepath, path.replace("%s/" % self.repobasename, ''))
+        return "%s/%s" % (self.urlbasepath, path.replace("%s/" % self.repobasepath, ''))
 
     def _repoIsPromoted(self, repo):
         if 'promotionpath' not in self.repoConfig[repo].keys():
