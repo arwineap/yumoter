43c6eef874320ee819ca91e24ad4fd05588e2fc5
aarwine <alex.arwine@evolemediallc.com>
2014-05-08 14:19:42 -0700

    Start making repo-gen sync nicely

--- repo-gen.py
+++ repo-gen.py
@@ -6,6 +6,9 @@ import yumoter
 yumoter = yumoter.yumoter('config/repos.json', '/home/aarwine/git/yumoter/repos')
 output = yumoter.syncRepos()
 
-print "errors:"
-for entry in output:
-    print entry[1]
+print "STDOUT"
+for entry in output[0]:
+	print entry
+print "STDERR"
+for entry in output[1]:
+	print entry
