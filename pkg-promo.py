3f98e07d25057bd09ccee0abd297e7a9802b261e
aarwine <alex.arwine@evolemediallc.com>
2014-05-09 15:14:47 -0700

    clarify usage

--- pkg-promo.py
+++ pkg-promo.py
@@ -14,11 +14,11 @@ import sys
 
 parser = argparse.ArgumentParser(description='The yumoter promo script will assist you in promoting pkgs and their dependencies through environments.')
 
-subparsers = parser.add_subparsers(help='sub-command help', dest='subprocess_name')
+subparsers = parser.add_subparsers(help='Use search to promote something, and list to get information about the repos', dest='subprocess_name')
 
 parser_list = subparsers.add_parser('list', help='List repo information')
 
-parser_search = subparsers.add_parser('search', help='search for pkgs')
+parser_search = subparsers.add_parser('search', help='Search for pkgs')
 parser_search.add_argument('search', help='specify a pkg to search for')
 parser_search.add_argument('-c', '--centosversion', help="Specify centos version", default="6.4")
 parser_search.add_argument('-e', '--environment', help="Specify enviornment / promotion path", required=True)
