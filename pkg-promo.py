fa7517b9f458e513bf864248fc5994e803242fe7
aarwine <alex.arwine@evolemediallc.com>
2014-05-09 15:04:00 -0700

    do the actual pkg promototion

--- pkg-promo.py
+++ pkg-promo.py
@@ -60,7 +60,6 @@ if promoteall.lower() != "y" and promoteall.lower() != "n":
     print "invalid selection"
     sys.exit(1)
 
-print "Here's the original pkg", searchPkgList[pkgChoice]
-print "Here's the dep pkgs", neededDeps[searchPkgList[pkgChoice]]
-
-print "ok, let's do stuff?"
+yumoter.promotePkg(searchPkgList[pkgChoice])
+yumoter.promotePkgs(neededDeps[searchPkgList[pkgChoice]])
+yumoter.createRepos()
