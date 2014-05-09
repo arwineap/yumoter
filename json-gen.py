87dba8ca4223ef79077cc44b8c373875b9dccd2a
aarwine <alex.arwine@evolemediallc.com>
2014-05-09 15:33:37 -0700

    Adding repos

--- json-gen.py
+++ json-gen.py
@@ -4,6 +4,10 @@ repos = {}
 reposbasepath = '/home/aarwine/projects/yumoter/repos'
 
 # static real path = "%s/%s/%s" % (reposbasepath, type, path)
+repos['os-65'] = {}
+repos['os-65']['upstream'] = 'rsync://archive.kernel.org/centos/6.5/os/x86_64/Packages/'
+repos['os-65']['path'] = 'os/6.5'
+repos['os-65']['osver'] = '6.5'
 repos['os-64'] = {}
 repos['os-64']['upstream'] = 'rsync://archive.kernel.org/centos/6.4/os/x86_64/Packages/'
 repos['os-64']['path'] = 'os/6.4'
@@ -12,6 +16,11 @@ repos['os-63'] = {}
 repos['os-63']['upstream'] = 'rsync://archive.kernel.org/centos/6.3/os/x86_64/Packages/'
 repos['os-63']['path'] = 'os/6.3'
 repos['os-63']['osver'] = '6.3'
+repos['os-56'] = {}
+repos['os-56']['upstream'] = 'rsync://archive.kernel.org/centos/5.6/os/x86_64/Packages/'
+repos['os-56']['path'] = 'os/5.6'
+repos['os-56']['osver'] = '5.6'
+
 
 repos['updates-6'] = {}
 repos['updates-6']['upstream'] = 'rsync://mirrors.kernel.org/centos/6/updates/x86_64/Packages/'
