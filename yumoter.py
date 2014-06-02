a6c7a9ce3836f54346983a12bb8aed1a602f73ec
aarwine <alex.arwine@evolemediallc.com>
2014-06-02 16:52:52 -0700

    Fixed doubleprinting of info

--- yumoter.py
+++ yumoter.py
@@ -277,7 +277,6 @@ class yumoter:
         print "deps list:"
         for deprepo in self.repoConfig[repo]['deprepos']:
             print "\tguessing repo:", deprepo
-            print "\tdep provided: %s" % deprepo
             print "\tdep found: %s" % self._magicTranslator(deprepo, osVer)
             #foo = self._magicTranslator(deprepo, osVer)
         #print "guessedName:"#, self._magicTranslator()
